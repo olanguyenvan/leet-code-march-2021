c7fca60a999f737a66e8e238f497c001ce26c95f
Ola NguyenVan <olanguyenvan@gmail.com>
2021-03-02 21:46:23 +0100

    Fix day02 and provide more tests

--- src/day02/repetitions.ts
+++ src/day02/repetitions.ts
@@ -1,7 +1,38 @@
-export function findErrorNums(nums: number[]): number[] {
-    for (let i: number = 0; i < nums.length; i++) {
-        if (nums[i] !== i + 1) {
-            return [nums[i], i + 1]
-        }
+export function findErrorNums(nums: number[]): [number, number] {
+    const numsLength: number = nums.length;
+    
+    if (numsLength < 2) {
+        throw new Error('Input was invalid. Minimum length of nums is 2.')
     }
+
+    nums.sort((a, b) => a - b);;
+
+    let duplicated: number = NaN;
+    let i = 1;
+
+    while (i < numsLength) {
+        if (nums[i] === nums[i - 1]) {
+            duplicated = nums[i];
+            break;
+        }
+        i+= 1;
+    }
+
+    let missed: number = 1;
+
+    i = 1;
+
+    while (i < numsLength) {
+        if (nums[i -1] + 2 === nums[i]) {
+            missed = nums[i] - 1;
+        }
+
+        i+= 1;
+    }
+    
+    if (nums[numsLength - 1] != numsLength) {
+        missed = numsLength;
+    }
+
+    return [duplicated, missed]
 };
