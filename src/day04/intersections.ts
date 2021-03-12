d21f5341d68c779b37cab521a38f17ff69e93205
Ola NguyenVan <olanguyenvan@gmail.com>
2021-03-12 23:23:34 +0100

    Add solution and tests for Day 04

--- src/day04/intersections.ts
+++ src/day04/intersections.ts
@@ -1,5 +1,37 @@
 import ListNode from './ListNode';
 
-function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+function getLength(head: ListNode | null) {
+    let length = 0;
+    
+    while (head) {
+        length++;
+        head = head.next;
+    }
 
+    return length;
+}
+
+export function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
+    const lengthA = getLength(headA);
+    const lengthB = getLength(headB);
+
+    let diff = lengthA - lengthB;
+    let nodeToAdvance: ListNode | null = diff > 0 ? headA : headB;
+
+    for (let i = 0; i < Math.abs(diff); i++) {
+        nodeToAdvance = nodeToAdvance.next;
+    } 
+
+    diff > 0 ? headA = nodeToAdvance :  headB = nodeToAdvance;
+    
+    while (headA && headB) {
+        if (headA === headB) {
+            return headA;
+        }
+
+        headA = headA.next;
+        headB = headB.next;
+    }
+
+    return null;
 };
